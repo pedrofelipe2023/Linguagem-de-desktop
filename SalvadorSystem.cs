a30a0ea91f3c87b9778af3d950b4ff637c8a4653
pedrofelipe2023 <150358360+pedrofelipe2023@users.noreply.github.com>
2024-08-08 22:04:16 -0300

    Add files via upload

---
+++ SalvadorSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.IO.Pipes;
+
+Console.Title = "Salvador System  APP C#";
+Console.WriteLine("Olá, Funcionário!");
+
+string Nome;
+string Cargo;
+Int32 AnoEntrada;
+Int32 AnoNascimento;
+Int32 AnoAtual;
+
+Console.Write("Digite seu nome completo: ");
+Nome = Console.ReadLine();
+Console.WriteLine("Seja bem vindo " + Nome);
+
+Console.Write("Informe o seu cargo: ");
+Cargo = Console.ReadLine();
+Console.WriteLine("Então esse é seu cargo: " + Cargo);
+
+Console.Write("Informe o seu ano de entrada na empresa: ");
+AnoEntrada = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o ano atual? ");
+AnoAtual = Convert.ToInt32(Console.ReadLine());
+Int32 TempoCalculado = AnoAtual - AnoEntrada;
+
+Console.WriteLine("Quantidade de anos que você trabalha na empresa: " + TempoCalculado);
+Console.Beep();
+
+Console.Write("digite o ano de nascimento: ");
+AnoNascimento = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o ano atual? ");
+AnoAtual = Convert.ToInt32(Console.ReadLine());
+
+Int32 IdadeCalculada = AnoAtual - AnoNascimento;
+
+Console.WriteLine("Você tem essa idade: ! " + IdadeCalculada);
+Console.Beep();
+
+decimal ValorSalário = 0;
+
+Console.Write("Informe seu Salário: ");
+ValorSalário = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informações recebidas com sucesso, Tenha um bom dia de trabalho!!! ");
+Console.Beep();
