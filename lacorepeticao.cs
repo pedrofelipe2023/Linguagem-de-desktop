f463d7d69c8f1ef04b5bcad84f25f6bf595d2745
pedrofelipe2023 <150358360+pedrofelipe2023@users.noreply.github.com>
2024-08-08 22:05:31 -0300

    Add files via upload

---
+++ lacorepeticao.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace laco_repeticao;
+class program
+{
+	static void Main(string[] args)
+	{
+		string nome = "Grau";
+
+
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine(nome);
+		}
+
+		Console.WriteLine("Laços de Repetição, Info07-M!");
+
+
+
+
+	}
+
+
+
+
+}
